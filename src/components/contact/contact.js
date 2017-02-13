78d4721094ea00e0f7d7e78a4bd40bd4780070ce
Vasin Max Limsukhawat <max.limsukhawat@qsc.com>
2017-02-12 19:24:12 -0700

    fixed

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
       <section className="contact-section">
         <div className="container">
           <div className="contact-row row text-center">
-            <div className="col-sm-6">
+            <div className="col-lg-6">
               <h2>Let's Talk</h2>
               <h4>If you want to turn your idea to reality, I can be your help or just want to know more about me that's fine too!</h4>
 
